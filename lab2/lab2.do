3dbcf506a9c0cfae1b11ccdef1c2e20c40a2349c
ise user <ise@localhost.localdomain>
2022-04-20 18:20:56 +0200

    A few changes

--- lab2/lab2.do
+++ lab2/lab2.do
@@ -1,11 +1,15 @@
 vcom -2008 devices.vhd gates.vhd lab2.vhd
 
-vsim work.counter
+vsim work.counter -debugdb
 
 add wav clk q tc rst ce
 
-force clk 0 0ns, 1 5ns -r 10ns
-force ce 0 0ns, 1 10ns
-force rst 1 0ns, 0 10ns
+force clk 0 0ns, 1 50ns -r 100ns
+force rst 1 0ns, 0 100ns
+force ce 0 0ns, 1 100ns
 
-run [expr 148080*10]ns
+run [expr 148080*100]ns
+
+wave zoom full
+
+//add schematic -full sim:/counter
--- lab2/lab2.vhd
+++ lab2/lab2.vhd
@@ -1,23 +1,5 @@
 library ieee;
 use ieee.std_logic_1164.all;
-
-entity decoder is			
-port(
-	data: in std_logic_vector(23 downto 0);
-	tc: out std_logic;
-	res: out std_logic
-);
-end entity;
-
-architecture behav of decoder is
-begin
-res <= '1' when data = x"148071" else '0';
-tc <= '1' when data = x"148070" else '0';
-end architecture behav;
-
-
-library ieee;
-use ieee.std_logic_1164.all;
 use work.devices_pkg.all;
 use work.gates_pkg.all;
 
@@ -26,32 +8,24 @@ entity counter is
 	ce: in std_logic;
 	clk: in std_logic;
 	rst: in std_logic;
-      	q: out std_logic_vector(23 downto 0);
-	tc: out std_logic
+      	q: inout std_logic_vector(23 downto 0);
+	tc: out std_logic;
+	res: out std_logic
 );
 end entity;
 
 architecture struct of counter is
- signal q_0, q_1, q_2, q_3, q_4, q_5: std_logic_vector(3 downto 0);
- signal ce_0, ce_1, ce_2, ce_3, ce_4, ce_5, tmp_res, tmp_res_2: std_logic;
- signal cur_q: std_logic_vector(23 downto 0);
+ signal ceo: std_logic_vector(5 downto 0);
+ signal dec_res, or_res: std_logic;
 
- component decoder
- port(
-	data: in std_logic_vector(23 downto 0);
-	tc: out std_logic;
-	res: out std_logic
- );
- end component;
 begin
-  inst_or: or2 generic map(Tpd => 0ns) port map(rst, tmp_res, tmp_res_2);
-  cntr_0: cntr_u generic map(T => 0ns) port map(tmp_res_2, ce, clk, ce_0, q_0);
-  cntr_1: cntr_u generic map(T => 0ns) port map(tmp_res_2, ce_0, clk, ce_1, q_1);
-  cntr_2: cntr_u generic map(T => 0ns) port map(tmp_res_2, ce_1, clk, ce_2, q_2);
-  cntr_3: cntr_u generic map(T => 0ns) port map(tmp_res_2, ce_2, clk, ce_3, q_3);
-  cntr_4: cntr_u generic map(T => 0ns) port map(tmp_res_2, ce_3, clk, ce_4, q_4);
-  cntr_5: cntr_u generic map(T => 0ns) port map(tmp_res_2, ce_4, clk, ce_5, q_5);
-  cur_q <= q_5 & q_4 & q_3 & q_2 & q_1 & q_0;
-  inst_decoder: decoder port map(cur_q, tc, tmp_res);
-  q <= cur_q;
+  tc <= '1' when q = x"148070" else '0';
+  res <= '1' when q = x"148071" else '0';
+  cntr_0: cntr_u generic map(T => 0ns) port map(or_res, ce, clk, ceo(0), q(3 downto 0));
+  cntr_1: cntr_u generic map(T => 0ns) port map(or_res, ceo(0), clk, ceo(1), q(7 downto 4));
+  cntr_2: cntr_u generic map(T => 0ns) port map(or_res, ceo(1), clk, ceo(2), q(11 downto 8));
+  cntr_3: cntr_u generic map(T => 0ns) port map(or_res, ceo(2), clk, ceo(3), q(15 downto 12));
+  cntr_4: cntr_u generic map(T => 0ns) port map(or_res, ceo(3), clk, ceo(4), q(19 downto 16));
+  cntr_5: cntr_u generic map(T => 0ns) port map(or_res, ceo(4), clk, ceo(5), q(23 downto 20));
+  or_gate: or2 generic map(Tpd => 0ns) port map(rst, dec_res, or_res);
 end architecture struct;
